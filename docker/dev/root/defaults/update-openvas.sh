7c786126c2d92d9d287c0d9279cf2ed1b7c11181
charlieIT <test@example.com>
2022-07-13 13:54:47 +0100

    Move scapdata-sync and nvt-sync to appropriate section

--- docker/dev/root/defaults/update-openvas.sh
+++ docker/dev/root/defaults/update-openvas.sh
@@ -3,6 +3,7 @@
 echo "Updating NVTs..."
 #s6-setuidgid abc greenbone-nvt-sync
 s6-setuidgid abc rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins
+s6-setuidgid abc greenbone-nvt-sync
 sleep 5
 
 rm /usr/local/var/run/feed-update.lock || true
@@ -15,6 +16,9 @@ rm /usr/local/var/run/feed-update.lock || true
 
 echo "Updating SCAP data..."
 s6-setuidgid abc greenbone-feed-sync --type SCAP
+s6-setuidgid abc greenbone-scapdata-sync --refresh-private
+echo "SCAP Selftest"
+s6-setuidgid abc greenbone-scapdata-sync --selftest
 sleep 5
 
 rm /usr/local/var/run/feed-update.lock || true
@@ -24,7 +28,3 @@ s6-setuidgid abc greenbone-feed-sync --type CERT
 sleep 5
 
 rm /usr/local/var/run/feed-update.lock || true
-
-s6-setuidgid abc greenbone-scapdata-sync --refresh
-s6-setuidgid abc greenbone-scapdata-sync --refresh-private
-s6-setuidgid abc greenbone-nvt-sync
